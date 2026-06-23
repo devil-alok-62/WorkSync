0348d437f49e9a844312d08c962de68e361e5ed1
devil-alok-62 <alokraj629937@email.com>
2026-06-23 09:53:18 +0530

    protect update

--- src/proxy.ts
+++ src/proxy.ts
@@ -30,7 +30,6 @@ export async function proxy(req: NextRequest) {
 }
 
 export const config = {
-  matcher: "/:path",
-
+  matcher: ["/dashboard/:path*"],
   //  " ((?!api|_next/static|_next/image|favicon.ico|node_modules).*)"
 };
